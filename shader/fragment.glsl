4a86ec0db6f780be13574bb2007a35058e34cc85
xubury <xubury97@gmail.com>
2021-04-14 10:41:51 +0800

    * pass color info

--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -2,6 +2,7 @@
 out vec4 fragColor;
 
 in vec2 texCoord;
+in vec3 color;
 
 uniform sampler2D texture0;
 
--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -4,6 +4,7 @@ layout(location = 1) in vec3 aColor;
 layout(location = 2) in vec2 aTexCoord;
 
 out vec2 texCoord;
+out vec3 color;
 
 uniform mat4 projection;
 uniform mat4 view;
@@ -12,5 +13,6 @@ uniform mat4 model;
 void main() {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
     texCoord = aTexCoord;
+    color = aColor;
 }
 
