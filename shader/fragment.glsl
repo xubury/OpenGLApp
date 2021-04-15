39a1684149e7cefddbc6476693cc22c5e1f894b8
xubury <xubury97@gmail.com>
2021-04-15 10:46:21 +0800

    * sepcular lighting

--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -6,19 +6,25 @@ in vec3 color;
 in vec2 texCoord;
 in vec3 normal;
 
-uniform vec3 lightPos;
 uniform vec3 lightColor;
+in vec3 lightPos;
 
 uniform sampler2D texture0;
 
 void main() {
-    vec3 lightDir = normalize(lightPos - fragPos);
-    vec3 diffuse = max(dot(normal, lightDir), 0.0) * lightColor;
-
     float ambientStrengh = 0.1;
     vec3 ambient = ambientStrengh * lightColor;
 
-    vec3 light = diffuse + ambient;
+    vec3 lightDir = normalize(lightPos - fragPos);
+    vec3 diffuse = max(dot(normal, lightDir), 0.0) * lightColor;
+
+    vec3 viewDir = normalize(-fragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float specularStrengh = 1.0;
+    float spec = pow(max(dot(viewDir, reflectDir), 0), 32);
+    vec3 sepecular = specularStrengh * spec * lightColor; 
+
+    vec3 light = ambient + diffuse + sepecular;
 
     fragColor = texture(texture0, texCoord) * vec4(light, 1.0);
 }
--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -13,12 +13,17 @@ uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model;
 
+uniform vec3 lightWorldPos;
+out vec3 lightPos;
+
 void main() {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
 
-    fragPos = vec3(model * vec4(aPos, 1.0));
+    fragPos = vec3(view * model * vec4(aPos, 1.0));
     color = aColor;
     texCoord = aTexCoord;
-    normal = normalize(mat3(transpose(inverse(model))) * aNormal);
+    normal = normalize(mat3(transpose(inverse(view * model))) * aNormal);
+
+    lightPos = vec3(view * vec4(lightWorldPos, 1.0));
 }
 
