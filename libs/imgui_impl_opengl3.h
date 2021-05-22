f2521b8e393f1c33e05c2b040467a3beadf54b05
xubury <xubury97@gmail.com>
2021-05-22 19:41:45 +0800

    * fix compile issue

--- libs/imgui_impl_opengl3.h
+++ libs/imgui_impl_opengl3.h
@@ -65,9 +65,7 @@ IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyDeviceObjects();
 
 // Otherwise try to detect supported Desktop OpenGL loaders..
 #elif defined(__has_include)
-#if __has_include(<GL/glew.h>)
-    #define IMGUI_IMPL_OPENGL_LOADER_GLEW
-#elif __has_include(<glad/glad.h>)
+#if __has_include(<glad/glad.h>)
     #define IMGUI_IMPL_OPENGL_LOADER_GLAD
 #elif __has_include(<glad/gl.h>)
     #define IMGUI_IMPL_OPENGL_LOADER_GLAD2
@@ -77,6 +75,8 @@ IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyDeviceObjects();
     #define IMGUI_IMPL_OPENGL_LOADER_GLBINDING3
 #elif __has_include(<glbinding/Binding.h>)
     #define IMGUI_IMPL_OPENGL_LOADER_GLBINDING2
+#elif __has_include(<GL/glew.h>)
+    #define IMGUI_IMPL_OPENGL_LOADER_GLEW
 #else
     #error "Cannot detect OpenGL loader!"
 #endif
