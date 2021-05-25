25fd60951655d14742a6930e48e1357c42b6447f
xubury <xubury97@gmail.com>
2021-05-25 13:01:48 +0800

    - comments

--- shader/primitiveVertex.glsl
+++ shader/primitiveVertex.glsl
@@ -10,10 +10,12 @@ uniform float screenHeight;
 void main() {
     vec4 screenPos = vec4(aPos, 1.0);
 
+    // convert to clip space
     screenPos.x = screenPos.x / screenWidth * 2 - 1; 
     screenPos.y = 1 - screenPos.y / screenHeight * 2;
 
     gl_Position = screenPos;
+
     color = aColor;
 }
 
