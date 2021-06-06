86e7af821fe277bf87355499cd4d763e4baf827c
xubury <xubury97@gmail.com>
2021-06-06 19:43:50 +0800

    * fix debug message

--- src/Graphic/GLContext.cpp
+++ src/Graphic/GLContext.cpp
@@ -4,32 +4,8 @@
 
 namespace te {
 
-static void glDebugMessage(GLenum, GLenum, GLuint, GLenum severity, GLsizei,
-                           const GLchar *message, const void *) {
-    switch (severity) {
-        case GL_DEBUG_SEVERITY_HIGH:
-            TE_CORE_CRITICAL("OpenGL Debug HIGH: {0}", message);
-            break;
-        case GL_DEBUG_SEVERITY_MEDIUM:
-            TE_CORE_CRITICAL("OpenGL Debug Medium: {0}", message);
-            break;
-        case GL_DEBUG_SEVERITY_LOW:
-            TE_CORE_CRITICAL("OpenGL Debug Low: {0}", message);
-            break;
-        case GL_DEBUG_SEVERITY_NOTIFICATION:
-            break;
-        default:
-            break;
-    }
-}
-
 void GLContext::init(glbinding::GetProcAddress procAddr) {
     glbinding::initialize(procAddr);
-
-    // Debug Message
-    glDebugMessageCallback(&glDebugMessage, nullptr);
-    glEnable(GL_DEBUG_OUTPUT);
-    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 
     glEnable(GL_DEPTH_TEST);
 
