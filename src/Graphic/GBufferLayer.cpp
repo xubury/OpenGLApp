b4eded6c2c136a88c5dddd363c1d7cf23b2003b4
xubury <xubury97@gmail.com>
2021-06-10 20:34:42 +0800

    * gbuffer

--- src/Graphic/GBufferLayer.cpp
+++ src/Graphic/GBufferLayer.cpp
@@ -6,11 +6,15 @@ GBufferLayer::GBufferLayer(int width, int height) {
     FrameBufferSpecification spec;
     spec.width = width;
     spec.height = height;
-    spec.attachmentsSpecs = {
-        {FramebufferTextureFormat::RGBA16F},
-        {FramebufferTextureFormat::RGBA16F},
-        {FramebufferTextureFormat::RGBA8},
-    };
+    spec.attachmentsSpecs = {{FramebufferTextureFormat::RGBA16F},
+                             {FramebufferTextureFormat::RGBA16F},
+                             {FramebufferTextureFormat::RGBA8},
+                             {FramebufferTextureFormat::DPETH}};
+    m_gBuffer = createScope<FrameBuffer>(spec, true);
 }
+
+void GBufferLayer::begin() {}
+
+void GBufferLayer::end() {}
 
 }  // namespace te
