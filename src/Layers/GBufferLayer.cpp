27a958ad9bfb7fb5bebf92a55ad3a1d89fa165ee
xubury <xubury97@gmail.com>
2021-06-14 15:31:35 +0800

    * gbuffer layer

--- src/Layers/GBufferLayer.cpp
+++ src/Layers/GBufferLayer.cpp
@@ -1,4 +1,4 @@
-#include "Graphic/GBufferLayer.hpp"
+#include "Layers/GBufferLayer.hpp"
 
 namespace te {
 
@@ -9,7 +9,7 @@ GBufferLayer::GBufferLayer(int width, int height) {
     spec.attachmentsSpecs = {{FramebufferTextureFormat::RGBA16F},
                              {FramebufferTextureFormat::RGBA16F},
                              {FramebufferTextureFormat::RGBA8},
-                             {FramebufferTextureFormat::DPETH}};
+                             {FramebufferTextureFormat::DEPTH32}};
     m_gBuffer = createScope<FrameBuffer>(spec, true);
 }
 
